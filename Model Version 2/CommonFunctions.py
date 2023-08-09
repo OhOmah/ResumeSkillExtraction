55aacd7e6df4c7f26066d154d4a0fb185c2b6464
Omar Zaffar <omar.zaffarr@gmail.com>
2023-08-09 12:51:34 -0400

    Updated code to adjust for new location of classes and functions.

--- Model Version 2/CommonFunctions.py
+++ Model Version 2/CommonFunctions.py
@@ -5,6 +5,7 @@ defined in this project.
 """
 from torch.utils.data import Dataset
 import torch
+import transformers
 
 
 class CustomDataset(Dataset):
@@ -40,3 +41,24 @@ class CustomDataset(Dataset):
             "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
             "targets": torch.tensor(self.targets[index], dtype=torch.float),
         }
+
+
+class BERTClass(torch.nn.Module, len):
+    def __init__(self):
+        # Defining the layers
+        super(BERTClass, self).__init__()
+        self.l1 = transformers.BertModel.from_pretrained(
+            "bert-base-uncased", return_dict=False
+        )
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(len, 22)
+
+    def forward(self, ids, mask, token_type_ids):
+        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        output_2 = self.l2(output_1)
+        output_3 = self.l3(output_2)
+        return output_3
+
+
+def loss_fn(outputs, targets):
+    return torch.nn.BCEWithLogitsLoss()(outputs, targets)
