16f76f39b43af9960810b169960e305f0d55f0de
Omar Zaffar <omar.zaffarr@gmail.com>
2023-09-21 13:36:52 -0700

    Started creation of the Resume Pasrser

--- Model Version 2/Resume_Parsing.py
+++
@@ -1,5 +0,0 @@
-'''
-PURPOSE OF THIS FILE:
-This file is a script to take in and parse a resume to pull the job titles listed. This script is served as a testing 
-playground for the final version used in the API. 
-'''
