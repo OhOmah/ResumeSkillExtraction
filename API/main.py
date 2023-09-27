898dbc247ec706dd9645068d58f6c7a645121bf7
Omar Zaffar <omar.zaffarr@gmail.com>
2023-09-26 18:40:15 -0700

    Started creation of React file upload

--- API/main.py
+++ API/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+from find_job_titles import FinderAcora
+from pdfminder.high_level import extract_text
+
+UPLOAD_DIR = Path() / 'uploads'
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/uploadfile/")
+async def create_upload_file(file_upload: UploadFile):
+
+    data = await file_upload.read()
+    save_to = UPLOAD_DIR / file_upload.filename
+    with open(save_to, 'wb') as f:
+        f.write(data)
+    
+
+
+    return {"filename":file_upload.filename} 
---
+++ API/resume-extract/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Fileform from './components/FileForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Fileform />
+    </div>
+  );
+}
+
+export default App;
